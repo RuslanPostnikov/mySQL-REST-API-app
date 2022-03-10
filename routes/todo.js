4a1f52cb26b9e5beb14ed05d63a80199b9b94434
RuslanPostnikov <rp02081992@gmail.com>
2022-03-10 17:51:30 +0200

    Task list

--- routes/todo.js
+++ routes/todo.js
@@ -1,57 +1,58 @@
-const {Router} = require('express');
-const Todo = require('../models/todo');
-const router = Router();
+const {Router} = require('express')
+const Todo = require('../models/todo')
+const router = Router()
 
-//Получение списка задач
-router.get('/', (req, res) => {
-    try {
+// Получение списка задач
+router.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll()
+    res.status(200).json(todos)
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
+})
 
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Server error'
-        })
-    }
-});
-
-//Создание новой задачи
+// Создание новой задачи
 router.post('/', async (req, res) => {
-    try {
-        const todo = await Todo.create({
-            title: req.body.title,
-            done: false
-        });
-        res.status(201).json({todo})
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Server error'
-        })
-    }
-});
+  try {
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false
+    })
+    res.status(201).json({todo})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
+})
 
 // Изменение задачи
-router.put('/;id', (req, res) => {
-    try {
+router.put('/:id', (req, res) => {
+  try {
 
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Server error'
-        })
-    }
-});
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
+})
 
 // Удаление задачи
-router.delete('/;id', (req, res) => {
-    try {
+router.delete('/:id', (req, res) => {
+  try {
 
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Server error'
-        })
-    }
-});
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
+})
 
-module.exports = router;
+module.exports = router
